d39820757d069ee2abf0a9e2589893d180117617
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2023-11-11 11:41:27 +0300

    add button up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./styles/style.css";
+
+let scrollButton = document.querySelector('.scroll-button');
+
+window.onscroll = function () {
+  if (window.scrollY > 800) {
+    scrollButton.classList.add('shown');
+  } else {
+    scrollButton.classList.remove('shown');
+  }
+};
+
+scrollButton.onclick = () => {
+    window.scrollTo(0, 0);
+}
