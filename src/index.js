bd9adf4231d71025e632cda11a954b31ec75a213
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2023-11-11 12:33:02 +0300

    add fixed menu

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import "./styles/style.css";
 
 let scrollButton = document.querySelector('.scroll-button');
+let headTop = document.querySelector(".head-top_white");
 
-window.onscroll = function () {
-  if (window.scrollY > 800) {
-    scrollButton.classList.add('shown');
-  } else {
-    scrollButton.classList.remove('shown');
-  }
-};
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 450) {
+      headTop.classList.add("shown-head");
+    } else {
+      headTop.classList.remove("shown-head");
+    }
+});
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 800) {
+      scrollButton.classList.add("shown");
+    } else {
+      scrollButton.classList.remove("shown");
+    }
+});
 
 scrollButton.onclick = () => {
     window.scrollTo(0, 0);
