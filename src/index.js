fdb638989c1482013d4c4de455c1e811ab3e52a7
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2023-11-17 12:33:52 +0300

    eddits

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ let scrollButton = document.querySelector(".scroll-button");
 let headTop = document.querySelector(".head-top");
 let select = document.querySelector("select");
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 450) {
-      headTop.classList.add("head-top_fixed");
-    } else {
-      headTop.classList.remove("head-top_fixed");
-    }
-});
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY > 450) {
+//       headTop.classList.add("head-top_fixed");
+//     } else {
+//       headTop.classList.remove("head-top_fixed");
+//     }
+// });
 window.addEventListener('scroll', () => {
     if (window.scrollY > 800) {
       scrollButton.classList.add("shown");
