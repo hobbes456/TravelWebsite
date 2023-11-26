a9f5a2557814e7b62cbb6b4db99171849025f341
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2023-11-26 17:16:46 +0300

    add filter class

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
 import "./styles/style.css";
 
 let headTopFixed = document.querySelector(".head-top_fixed");
+let menuNavigation = document.querySelector(".menu__navigation");
 let select = document.getElementById("path");
 let dateTo = document.getElementById("date-to");
 let dateFrom = document.getElementById("date-from");
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
     if (window.scrollY > 450) {
-      headTopFixed.classList.add("shown");
+        headTopFixed.classList.add("shown");
     } else {
-      headTopFixed.classList.remove("shown");
+        headTopFixed.classList.remove("shown");
+    }
+});
+
+menuNavigation.addEventListener("click", e => {
+    if (e.target.tagName === "A") {
+        for (let a of menuNavigation.children) {
+            a.classList.remove("valid");
+        }
+        e.target.classList.add("valid");
     }
 });
 
 select.onchange = () => {
-  select.style.color = "#000";
-  // ничего умнее не нашел
-}
+    select.style.color = "#000";
+    // ничего умнее не нашел
+};
 
 dateTo.onchange = () => {
-  dateTo.style.color = "#000";
-}
+    dateTo.style.color = "#000";
+};
 
 dateFrom.onchange = () => {
-  dateFrom.style.color = "#000";
-}
+    dateFrom.style.color = "#000";
+};
