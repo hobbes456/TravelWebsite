1bc4c94e0426708981ba20a9da98a80bff4b0078
Ilia Ivchenkov <i.iw4enckov@yandex.ru>
2023-11-29 22:46:05 +0300

    add change on items form and event on reset

--- src/index.js
+++ src/index.js
@@ -3,43 +3,51 @@ import IMask from "imask";
 
 let headTopFixed = document.querySelector(".head-top_fixed");
 let menuNavigation = document.querySelector(".menu__navigation");
+
 let select = document.getElementById("path");
 let telefon = document.getElementById("tel");
 let dateTo = document.getElementById("date-to");
 let dateFrom = document.getElementById("date-from");
+let resetBtn = document.getElementById("reset-btn");
+
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 450) {
-    headTopFixed.classList.add("shown");
-  } else {
-    headTopFixed.classList.remove("shown");
-  }
+    if (window.scrollY > 450) {
+        headTopFixed.classList.add("shown");
+    } else {
+        headTopFixed.classList.remove("shown");
+    }
 });
 
 menuNavigation.addEventListener("click", (e) => {
-  if (e.target.tagName === "A") {
-    for (let a of menuNavigation.children) {
-      a.classList.remove("valid");
+    if (e.target.tagName === "A") {
+        for (let a of menuNavigation.children) {
+        a.classList.remove("valid");
+        }
+        e.target.classList.add("valid");
     }
-    e.target.classList.add("valid");
-  }
 });
 
-select.onchange = () => {
-  select.style.color = "#000";
-  // ничего умнее не нашел
-};
+select.addEventListener("change", () => {
+    select.classList.add("black");
+})
+
+dateTo.addEventListener("change", () => {
+    dateTo.classList.add("black");
+})
+
+dateFrom.addEventListener("change", () => {
+    dateFrom.classList.add("black");
+})
+
+resetBtn.addEventListener("click", () => {
+    select.classList.remove("black");
+    dateTo.classList.remove("black");
+    dateFrom.classList.remove("black");
+});
 
 const maskOptions = {
     mask: '+{7}-000-000-00-00'
 }
 
 IMask(telefon, maskOptions);
-
-dateTo.onchange = () => {
-  dateTo.style.color = "#000";
-};
-
-dateFrom.onchange = () => {
-  dateFrom.style.color = "#000";
-};
